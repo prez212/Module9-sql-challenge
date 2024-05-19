8c62f3013b74c45a66ebbcd505299c90f14d9025
Lucas Perez <you@example.com>
2024-05-19 11:11:48 -0400

    update Module9schema and Module9_Data_Analysis

--- Module9schema.sql
+++ Module9schema.sql
@@ -13,44 +13,51 @@ DROP TABLE IF EXISTS salary;
 
 -- Create new tables to import data
 CREATE TABLE departments (
-	dept_no	VARCHAR(10) NOT NULL, 
-	dept_name TEXT NOT NULL PRIMARY KEY
+	dept_no	VARCHAR NOT NULL PRIMARY KEY, 
+	dept_name VARCHAR NOT NULL
 );
 
 -- Create new table to import data
 CREATE TABLE dept_emp (
-	emp_no	int NOT NULL, 
-	dept_no VARCHAR (10) NOT NULL,
+	emp_no	INT NOT NULL, 
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
 
 -- Create new table to import data
 CREATE TABLE dept_manager (
-	dept_no VARCHAR (20) NOT NULL, 
-	emp_no	INT PRIMARY KEY
+	dept_no VARCHAR NOT NULL, 
+	emp_no	INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 
 -- Create new table to import data
 CREATE TABLE title (
-	title_id VARCHAR (20) NOT NULL PRIMARY KEY,
+	title_id VARCHAR NOT NULL PRIMARY KEY,
 	title TEXT NOT NULL
 );
 
 -- Create new table to import data
 CREATE TABLE employees (
 	emp_no	INT NOT NULL PRIMARY KEY,
-	emp_title_id VARCHAR (20) NOT NULL,
-	birth_date VARCHAR (20) NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date VARCHAR NOT NULL,
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
 	sex TEXT NOT NULL,
-	hire_date VARCHAR(20) NOT NULL
+	hire_date VARCHAR NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES title (title_id)
 );
 
 -- Create new table to import data
-CREATE TABLE salary (
+CREATE TABLE salaries (
 	emp_no	INT NOT NULL PRIMARY KEY,
-	salary INT NOT NULL
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
